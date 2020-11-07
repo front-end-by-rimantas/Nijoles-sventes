089d1703268e4aa60b352a2c3d7608c2965f6585
Rimantas <belauzas@yahoo.com>
2020-11-07 14:26:07 +0200

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,11 @@
+const sventes = [
+    {
+        name: 'Nauji metai',
+        date: {
+            month: 1,
+            day: 1
+        }
+    },
+];
+
+export { sventes }
---
+++ js/generateSventes.js
@@ -0,0 +1,5 @@
+function generateSventes() {
+    // logic
+}
+
+export { generateSventes }
