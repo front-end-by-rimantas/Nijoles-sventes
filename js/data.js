7e2b3961e417553cb57985cf7715c43707a3d475
Rimantas <belauzas@yahoo.com>
2020-11-07 14:48:32 +0200

    Naujos sventes

--- js/data.js
+++ js/data.js
@@ -6,6 +6,83 @@ const sventes = [
             day: 1
         }
     },
+    {
+        name: 'Lietuvos valstybės atkūrimo diena',
+        date: {
+            month: 2,
+            day: 16
+        }
+    },
+    {
+        name: 'Lietuvos nepriklausomybės atkūrimo diena',
+        date: {
+            month: 3,
+            day: 11
+        }
+    },
+    {
+        name: 'Tarptautinė darbo diena',
+        date: {
+            month: 5,
+            day: 1
+        }
+    },
+    {
+        name: 'Rasos (Joninės)',
+        date: {
+            month: 6,
+            day: 24
+        }
+    },
+    {
+        name: 'Valstybės (Lietuvos karaliaus Mindaugo karūnavimo) diena',
+        date: {
+            month: 7,
+            day: 6
+        }
+    },
+    {
+        name: 'Žolinė; Švč. Mergelės Marijos ėmimo į dangų diena',
+        date: {
+            month: 8,
+            day: 15
+        }
+    },
+    {
+        name: 'Visų šventųjų diena',
+        date: {
+            month: 11,
+            day: 1
+        }
+    },
+    {
+        name: 'Vėlinės',
+        date: {
+            month: 11,
+            day: 2
+        }
+    },
+    {
+        name: 'Kūčios',
+        date: {
+            month: 12,
+            day: 24
+        }
+    },
+    {
+        name: 'Kalėdos',
+        date: {
+            month: 12,
+            day: 25
+        }
+    },
+    {
+        name: 'Kalėdos',
+        date: {
+            month: 12,
+            day: 26
+        }
+    },
 ];
 
 export { sventes }
